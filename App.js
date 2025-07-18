ffa1c8064229caa3af73099f9d51e343bdb27d3c
development@suthratech <development@suthratech.com>
2025-07-18 23:00:35 +0530

    start the server with parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+        import React from "react";
+        import ReactDOM from "react-dom/client";
+
 
       const headingEl = React.createElement("h1", {}, "Revising React");
       const root = ReactDOM.createRoot(document.getElementById("root")); // this reactDOM api helping to render the elemnt in the root div and displaying it on the browser
